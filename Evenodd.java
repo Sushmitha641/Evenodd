d6a2c281917358f5c74bba44f46f45c1f253a059
Sushmitha641 <245122748027@mvsrec.edu.in>
2025-03-18 12:54:42 +0530

    Update Evenodd.java

--- Evenodd.java
+++ Evenodd.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EvenOdd {
+public class Evenodd {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
